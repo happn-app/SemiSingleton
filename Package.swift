88884a40793be19e13b42adcc000e0d5795947b7
Frizlab <fload@me.com>
2019-03-31 20:48:02 +0200

    Update project to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,30 +1,18 @@
-// swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.0
 import PackageDescription
-
 
 
 let package = Package(
 	name: "SemiSingleton",
 	products: [
-		.library(
-			name: "SemiSingleton",
-			targets: ["SemiSingleton"]
-		)
+		.library(name: "SemiSingleton", targets: ["SemiSingleton"]),
 	],
 	dependencies: [
-		.package(url: "git@github.com:happn-app/DummyLinuxOSLog.git", from: "1.0.0"),
-		.package(url: "git@github.com:happn-app/RecursiveSyncDispatch.git", from: "1.0.0")
+		.package(url: "https://github.com/happn-tech/DummyLinuxOSLog.git", from: "1.0.1"),
+		.package(url: "https://github.com/happn-tech/RecursiveSyncDispatch.git", from: "1.0.0")
 	],
 	targets: [
-		.target(
-			name: "SemiSingleton",
-			dependencies: ["DummyLinuxOSLog", "RecursiveSyncDispatch"]
-		),
-		.testTarget(
-			name: "SemiSingletonTests",
-			dependencies: ["SemiSingleton"]
-		)
+		.target(name: "SemiSingleton", dependencies: ["DummyLinuxOSLog", "RecursiveSyncDispatch"]),
+		.testTarget(name: "SemiSingletonTests", dependencies: ["SemiSingleton"])
 	]
 )
